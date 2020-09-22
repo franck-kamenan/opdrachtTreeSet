3e60618c027df8222bb2f7c4c728b00fc7f7415c
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-22 11:23:17 +0200

    TreeSet SortedSet first

---
+++ TreeApp.java
@@ -0,0 +1,18 @@
+package be.intecbrussel.opdrachten.opdrachttreeapp;
+
+import java.util.*;
+
+public class TreeApp {
+
+    public static void main(String[] args) {
+
+        SortedSet<Integer> intSet = new TreeSet<>();
+        intSet.add(15);
+        intSet.add(14);
+        intSet.add(19);
+        intSet.add(24);
+        intSet.add(21);
+
+        System.out.println(intSet.first());
+    }
+}
